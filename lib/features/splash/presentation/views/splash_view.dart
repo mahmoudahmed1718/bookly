55c6bf0972bce9d7e8f39fbe61847fb4c9c4dea7
mahmoud ahmed <mahmoudahmedhsn@gmail>
2024-11-20 07:09:53 +0200

    fist

---
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,12 @@
+import 'package:bookly/features/splash/presentation/views/widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class Splashview extends StatelessWidget {
+  const Splashview({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bookly/constant.dart';
+import 'package:bookly/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kprimarycolor),
+      home: const Splashview(),
+    );
+  }
+}
