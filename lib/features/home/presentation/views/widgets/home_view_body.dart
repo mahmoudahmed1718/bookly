bd3bb717b775cbe5e6e42bfe16cb7ef4d8b3d171
mahmoud ahmed <mahmoudahmedhsn@gmail>
2024-11-20 18:12:53 +0200

    r

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/features/home/presentation/views/widgets/cusotm_appbar.dart';
+import 'package:bookly/features/home/presentation/views/widgets/featured_books_listview.dart';
 import 'package:bookly/features/home/presentation/views/widgets/featured_listview_item.dart';
 import 'package:flutter/material.dart';
 
@@ -7,24 +8,10 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [CustomAppbar(), FeaturedBooksListView()],
-    );
-  }
-}
-
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8),
-                child: FeaturedListViewItem());
-          }),
+      children: [
+        CustomAppbar(),
+        FeaturedBooksListView(),
+      ],
     );
   }
 }
