a9370470ad3d12f764acaf7f79c713ca5301600e
mahmoud ahmed <mahmoudahmedhsn@gmail>
2024-11-22 13:39:45 +0200

    add font famly

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookly/constant.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,8 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kprimarycolor,
-      ),
+          scaffoldBackgroundColor: kprimarycolor,
+          textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)
+          //
+          ),
       home: const Splashview(),
     );
   }
