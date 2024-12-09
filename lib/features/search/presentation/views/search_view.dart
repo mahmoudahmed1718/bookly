8ca82d9af9027549a661bb9b4028761f930b0f56
mahmoud ahmed <mahmoudahmedhsn@gmail>
2024-12-09 17:03:59 +0200

    create book result

--- lib/features/search/presentation/views/search_view.dart
+++ lib/features/search/presentation/views/search_view.dart
@@ -1,20 +1,31 @@
+import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/search/presentation/views/widgets/book_result.dart';
 import 'package:bookly/features/search/presentation/views/widgets/custom_search_text_field.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_icon_class/font_awesome_icon_class.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: const Padding(
+    return const Scaffold(
+        body: Padding(
       padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
           SizedBox(
             height: 24,
           ),
-          CustomSearchTextField()
+          CustomSearchTextField(),
+          SizedBox(
+            height: 16,
+          ),
+          Align(
+              alignment: Alignment.centerLeft,
+              child: Text('Best Seller', style: Styles.fontsize18)),
+          Expanded(child: BookResult()),
+          SizedBox(
+            height: 16,
+          ),
         ],
       ),
     ));
--- lib/features/search/presentation/views/widgets/book_result.dart
+++ lib/features/search/presentation/views/widgets/book_result.dart
@@ -1,14 +1,13 @@
 import 'package:bookly/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:flutter/material.dart';
 
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
+class BookResult extends StatelessWidget {
+  const BookResult({super.key});
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
         shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        itemCount: 10,
+        itemCount: 15,
         itemBuilder: (context, index) {
           return const Padding(
             padding: EdgeInsets.symmetric(vertical: 10),
