f5be97dc18d9b0ea1fc06fd0cddeb128f1a6c196
mahmoud ahmed <mahmoudahmedhsn@gmail>
2024-12-11 14:35:04 +0200

    create home repo method and faileur file

---
+++ lib/core/errors/faileur.dart
@@ -0,0 +1,4 @@
+// ignore: file_names
+abstract class Faileur {}
+
+class ServerFaileur extends Faileur {}
--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly/core/errors/faileUr.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSelletBooks();
-  fetechFeatureBook();
+  Future<Either<Faileur, List<BookModel>>> fetchBestSelletBooks();
+  Future<Either<Faileur, List<BookModel>>> fetechFeatureBook();
 }
