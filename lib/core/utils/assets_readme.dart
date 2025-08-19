c68e3f24e7d5ee3b001c57ee89833b45701ba6f6
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-19 17:43:57 +0300

    add image for readme file

---
+++ lib/core/utils/assets_readme.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImageReadmeBookDetail
+  /// assets/image_readme/book_detail.jpg
+  static const String assetsImageReadmeBookDetail = "assets/image_readme/book_detail.jpg";
+
+  /// Assets for assetsImageReadmeHome
+  /// assets/image_readme/home.jpg
+  static const String assetsImageReadmeHome = "assets/image_readme/home.jpg";
+
+  /// Assets for assetsImageReadmeSpalsh
+  /// assets/image_readme/spalsh.jpg
+  static const String assetsImageReadmeSpalsh = "assets/image_readme/spalsh.jpg";
+}
+
